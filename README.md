afcf866bae05ebd1d7687b487c9b93c04ef2c5d3
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-08-31 17:44:25 +0200

    done

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # acty-front-end-test
 
+This test should take around 1-2 hour to complete. Try not to use more than two hours on this test.
+
+### Task
+
+* Create a new project using technologies that you are comfortable with (ReactJs, Vue.js, Angular, VanillaJS). 
+* Upload the project to GitHub or BitBucket and then send the link when you are done.
+
+## Resources
+
+Create a page that looks like this design uploaded to figma (You might need to sign in to Figma to see all features of preview) https://www.figma.com/file/ODCUfAncluQWKm9IAbKjEy/ESG-Scoring-for-test?node- id=14%3A2
+
+We’ve provided an API endpoint for the data included in the design file here
+https://api.acty.se/
+
+# Commands
+
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <h1>ESG Scoring</h1>
+      <p>This is a list of top 5 companies in each ESG category</p>
+      <div class="category-wrapper">
+        <p class="category-heading"><span></span>Category</p>
+        <p v-for="obj in info" v-bind:key="obj.id">
+          {{ obj.score }} {{ obj.name }} {{ obj.categories }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.acty.se/")
+      .then((response) => (this.info = response.data));
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
 }
+
+.wrapper {
+  max-width: 37.5rem;
+  margin: auto;
+  padding-top: 1.5rem;
+}
+
+h1 {
+  font-size: 1.125rem;
+  padding-bottom: 0.5rem;
+}
+
+p {
+  font-size: 0.88rem;
+  line-height: 1.5rem;
+}
+
+
+.category-heading {
+  display: flex;
+  align-items: center;
+}
+
+span {
+  height: 1rem;
+  width: 1rem;
+  background-color: #9FD3A5;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0 .3rem 0 0;
+}
+
+
+.category-wrapper {
+  background-color: #f5ffee;
+  border-radius: 1rem;
+  padding: 1rem 1.5rem;
+  margin-top: 1rem;
+}
+
+
+
+
 </style>
