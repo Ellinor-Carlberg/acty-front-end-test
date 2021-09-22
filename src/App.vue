48b2b4e68d40f933ad38827c297adda80afaf6d6
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-09-22 14:28:05 +0200

    display by each category works

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,51 @@
     <div class="wrapper">
       <h1>ESG Scoring</h1>
       <p>This is a list of top 5 companies in each ESG category</p>
-      <div class="category-wrapper">
-        <p class="category-heading"><span></span>Category</p>
-        <p v-for="obj in info" v-bind:key="obj.id">
-          {{ obj.score }} {{ obj.name }} {{ obj.categories }}
-        </p>
-      </div>
+      <category-card v-bind:category="environmental" title="Environmental" />
+      <category-card v-bind:category="social" title="Social" />
+      <category-card v-bind:category="governance" title="Governance" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import CategoryCard from "./components/CategoryCard.vue";
+
 export default {
+  components: { CategoryCard },
   name: "App",
 
   data() {
     return {
-      info: null
+      environmental: [],
+      social: [],
+      governance: [],
     };
   },
   mounted() {
     axios
       .get("https://api.acty.se/")
-      .then((response) => (this.info = response.data));
+      .then((response) => {
+        const arrayOfObjects = response.data;
+
+        //filter by each category, then sort by score(highest to lowest)
+        this.environmental = arrayOfObjects.filter(
+          (obj) => obj.categories === "Environmental"
+        );
+        this.environmental.sort((a, b) => b.score - a.score);
+
+        this.social = arrayOfObjects.filter(
+          (obj) => obj.categories === "Social"
+        );
+        this.social.sort((a, b) => b.score - a.score);
+
+        this.governance = arrayOfObjects.filter(
+          (obj) => obj.categories === "Governance"
+        );
+        this.governance.sort((a, b) => b.score - a.score);
+      })
+      .catch((error) => console.error(error));
   },
 };
 </script>
@@ -40,9 +61,12 @@ export default {
 }
 
 .wrapper {
-  max-width: 37.5rem;
+  width: 37.5rem;
+  max-width: 90vw;
   margin: auto;
   padding-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
 }
 
 h1 {
@@ -54,31 +78,4 @@ p {
   font-size: 0.88rem;
   line-height: 1.5rem;
 }
-
-
-.category-heading {
-  display: flex;
-  align-items: center;
-}
-
-span {
-  height: 1rem;
-  width: 1rem;
-  background-color: #9FD3A5;
-  border-radius: 50%;
-  display: inline-block;
-  margin: 0 .3rem 0 0;
-}
-
-
-.category-wrapper {
-  background-color: #f5ffee;
-  border-radius: 1rem;
-  padding: 1rem 1.5rem;
-  margin-top: 1rem;
-}
-
-
-
-
 </style>
---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="category-wrapper"
+    v-bind:class="[
+      { yellow: title === 'Social' },
+      { blue: title === 'Governance' },
+    ]"
+  >
+    <p class="category-heading"><span></span>{{ title }}</p>
+    <p v-for="obj in category" v-bind:key="obj.id">
+      {{ obj.score }} {{ obj.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryCard",
+  props: ["category", "title"],
+};
+</script>
+
+<style scoped>
+.category-wrapper {
+  background-color: #f5ffee;
+  border-radius: 1rem;
+  padding: 1rem 1.5rem;
+  margin-top: 1rem;
+}
+
+.yellow {
+  background-color: #fffff1;
+}
+
+.blue {
+  background-color: #f2f7ff;
+}
+
+.category-heading {
+  display: flex;
+  align-items: center;
+}
+
+span {
+  height: 1rem;
+  width: 1rem;
+  background-color: #9fd3a5;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0 0.3rem 0 0;
+}
+
+.yellow span {
+  background-color: #f2e888;
+}
+
+.blue span {
+  background-color: #88bff2;
+}
+</style>
